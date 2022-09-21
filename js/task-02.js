1b073a09a3249d1ccfd788b104c26c80053a4709
Den4kss <106112858+Den4kss@users.noreply.github.com>
2022-09-22 00:30:16 +0300

    3

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredientsId = document.querySelector("#ingredients");
+  const ingredientEl = document.createElement("li");
+
+  ingredientEl.classList.add("item");
+  ingredientEl.textContent = `${ingredients[i]}`;
+  ingredientsId.append(ingredientEl);
+}
