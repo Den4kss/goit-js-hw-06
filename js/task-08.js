53001b86e478871acbea45a1bf5b3b998360ae7c
Den4kss <106112858+Den4kss@users.noreply.github.com>
2022-10-02 23:29:00 +0300

    ...

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
+console.log(formEl);
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formNameEl = event.target.elements;
+  if (
+    formNameEl.email.value.length > 0 &&
+    formNameEl.password.value.length > 0
+  ) {
+    const data = {
+      email: formNameEl.email.value,
+      password: formNameEl.password.value,
+    };
+    console.log(data);
+  } else {
+    alert("Всі поля повинні бути заповнені!");
+  }
+
+  formEl.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+bodyEl.classList.add("body__style");
+const colorEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", function () {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorEl.textContent = `${getRandomHexColor()}`;
+});
